33acc574d9118faee53e2a7a5908f4bf08d0e17b
Jorge Castro Gallardo <jcastro@ujaen.es>
2016-06-14 16:23:43 +1000

    Set exit on fail to false

--- src/main/java/delfos/web/database/item/PrintItemSet.java
+++ src/main/java/delfos/web/database/item/PrintItemSet.java
@@ -7,6 +7,7 @@ package delfos.web.database.item;
 
 import delfos.CommandLineParametersError;
 import delfos.ConsoleParameters;
+import delfos.Constants;
 import delfos.dataset.basic.item.Item;
 import delfos.dataset.changeable.ChangeableDatasetLoader;
 import delfos.main.managers.database.DatabaseManager;
@@ -34,6 +35,9 @@ public class PrintItemSet {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String printItemSetHTML() {
+
+        Constants.setExitOnFail(false);
+
         try {
             List<Item> itemSet = getItemSet();
             JsonArray items = ItemJson.getItemsArray(itemSet);
@@ -47,6 +51,9 @@ public class PrintItemSet {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String printItemSetJson() {
+
+        Constants.setExitOnFail(false);
+
         try {
             List<Item> itemSet = getItemSet();
             JsonArray items = ItemJson.getItemsArray(itemSet);
